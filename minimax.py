d7ed50ba275ca660792a3946de7276d467b88484
Marco Selvatici <marcoselvatici98@gmail.com>
2018-04-16 01:35:28 +0200

    add is_win_fast optimization

--- minimax.py
+++ minimax.py
@@ -35,12 +35,8 @@ is represented as:
 """
 class Game_state:
     def __init__(self, state=None, heights=None):
-        if state is None or heights is None:
-            self.state   = [0 for _ in range(42)]
-            self.heights = [0 for _ in range(7)]
-        else:
-            self.state   = state
-            self.heights = heights 
+        self.state   = [0 for _ in range(42)]
+        self.heights = [0 for _ in range(7)]
 
     def col_to_idx(self, col):
         return (7 * (6 - self.heights[col])) + col
@@ -52,15 +48,11 @@ class Game_state:
         return (int(idx / 7), idx % 7)
     
     def insert_coin(self, pos, color):
-        if pos < 0 or pos > 6 or self.heights[pos] >= 6:
-            raise ValueError("Trying to insert a coin out of limits")
-        self.heights[pos] += 1          # the order of this two operations is critical
+        self.heights[pos] += 1 # the order of this two operations is critical
         self.state[self.col_to_idx(pos)] = color
 
     def remove_coin(self, pos):
-        if pos < 0 or pos > 6 or self.heights[pos] <= 0:
-            raise ValueError("Trying to insert a coin out of limits")
-        self.state[self.col_to_idx(pos)] = 0      # the order of this two operations is critical
+        self.state[self.col_to_idx(pos)] = 0 # the order of this two operations is critical
         self.heights[pos] -= 1
 
     def check_connect4(self, f1, f2, i, j):
@@ -90,32 +82,28 @@ class Game_state:
                         return True
         return False
 
-    """
-    have to consider the case in wich I am in between
-    """
+    def count_connected(self, f1, f2, i, j):
+        tmp = 0
+        for k in range(1, 4): # [1,2,3]
+            if i + k*f1 >= 0 and i + k*f1 < 6 and j + k*f2 >= 0 and j + k*f2 < 7 and \
+               self.state[self.ij_to_idx( i + k*f1, j + k*f2 )] == self.state[self.ij_to_idx(i,j)]:
+                tmp += 1
+            else:
+                break
+        return tmp
+    
     def is_win_fast(self, col):
         i, j = self.idx_to_ij(self.col_to_idx(col))
-    	# horizontal
-        if (j + 3 < 7) and self.check_connect4(0, 1, i, j):
-            return True
-        if (j - 3 >= 0) and self.check_connect4(0, -1, i, j):
+        # horizontal
+        if(self.count_connected(0, 1, i, j) + self.count_connected(0, -1, i, j) + 1 >= 4):
             return True
         # vertical
-        if (i + 3 < 6) and self.check_connect4(1, 0, i, j):
+        if(self.count_connected(1, 0, i, j) + self.count_connected(-1, 0, i, j) + 1 >= 4):
             return True
-        if (i - 3 >= 0) and self.check_connect4(-1, 0, i, j):
+        # oblique 
+        if(self.count_connected(1, 1, i, j) + self.count_connected(-1, -1, i, j) + 1 >= 4):
             return True
-        # oblique down right
-        if (i + 3 < 6 and j + 3 < 7) and self.check_connect4(1, 1, i, j):
-            return True
-        # oblique down left
-        if (i + 3 < 6 and j - 3 >= 0) and self.check_connect4(1, -1, i, j):
-            return True
-        # oblique up right
-        if (i - 3 >= 0 and j + 3 < 7) and self.check_connect4(-1, 1, i, j):
-            return True
-        # oblique up left
-        if (i - 3 >= 0 and j - 3 >= 0) and self.check_connect4(-1, -1, i, j):
+        if(self.count_connected(-1, 1, i, j) + self.count_connected(1, -1, i, j) + 1 >= 4):
             return True
         return False
 
@@ -216,10 +204,12 @@ class Minimax_agent:
 
             state.insert_coin(col, self.PLAYER1) # insert coin and go down with recursion
             # chek if this move lead to a win
-            if state.is_win():
-            	max_val = self.discount(self.WIN, depth)
+            if state.is_win_fast(col):
+                #max_val = max(max_val, self.discount(self.WIN, depth + 1))
+                state.remove_coin(col)
+                return self.discount(self.WIN, depth + 1)
             else: # if not, we have to calculate everything normally
-            	max_val = max(max_val, self.min_node(state, depth + 1, alpha, beta))
+                max_val = max(max_val, self.min_node(state, depth + 1, alpha, beta))
             state.remove_coin(col) # remove coin to get the previous state
 
             if max_val > beta:
@@ -241,10 +231,12 @@ class Minimax_agent:
 
             state.insert_coin(col, self.PLAYER2) # insert coin and go on with recursion   
             # chek if this move lead to a win
-            if state.is_win():
-            	min_val = self.discount(-self.WIN, depth)
+            if state.is_win_fast(col):
+                #min_val = min(min_val, self.discount(-self.WIN, depth))
+                state.remove_coin(col)
+                return self.discount(-self.WIN, depth)
             else: # if not, we have to calculate everything normally
-            	min_val = min(min_val, self.max_node(state, depth + 1, alpha, beta))
+                min_val = min(min_val, self.max_node(state, depth + 1, alpha, beta))
             state.remove_coin(col) # remove coin to get the previous state
 
             if min_val < alpha:
@@ -265,12 +257,17 @@ class Minimax_agent:
         start = time.time()
 
         max_val = -self.INF
+        alpha = -self.INF
         best_moves = []
         for col in self.columns:
             if not self.can_insert_coin(state, col):
                 continue
+            tmp = 0
             state.insert_coin(col, self.PLAYER1)
-            tmp = self.min_node(state, 1, -self.INF, self.INF)
+            if state.is_win_fast(col):
+                tmp = self.WIN
+            else:
+                tmp = self.min_node(state, 1, alpha, self.INF)
             state.remove_coin(col)
             
             print(tmp, end=" ")
@@ -279,6 +276,7 @@ class Minimax_agent:
             if tmp > max_val:
                 best_moves.clear()
                 max_val = tmp
+                alpha = tmp
                 best_moves.append(col)
             elif tmp == max_val:
                 best_moves.append(col)
@@ -296,13 +294,29 @@ def main():
     #  3   |   5
     # ...
 
-    """
+    
     curr_state = Game_state()
-    for _ in range(4):
+    """ oblique
+    for _ in range(3):
         curr_state.insert_coin(3, 1)
+    for _ in range(2):
+        curr_state.insert_coin(4, 1)
+    curr_state.insert_coin(5, 1)
 
+    for i in range(4):
+        curr_state.insert_coin(3 + i, 2)
+    
+    # other oblique
+    for _ in range(3):
+        curr_state.insert_coin(6, 1)
+    for _ in range(2):
+        curr_state.insert_coin(5, 1)
+    curr_state.insert_coin(4, 1)
+
+    for i in range(4):
+        curr_state.insert_coin(3 + i, 2)
+    
     curr_state.print()
-    print("idx:", curr_state.col_to_idx(3), flush=True)
     print(curr_state.is_win_fast(3))
 
     """
@@ -317,7 +331,10 @@ def main():
     turn = 0 # human
     while not curr_state.is_win() and turn < 42:
         if turn % 2 == 0:
-            pos = int(input("your move: "))
+            pos = int(input("Your move: "))
+            while pos < 0 or pos > 6 or curr_state.heights[pos] >= 6:
+                print("Trying to insert a coin in an invalid position")
+                pos = int(input("Your move: "))
             curr_state.insert_coin(pos, 2)
         else:
             pos = agent.get_move(curr_state)
@@ -327,6 +344,7 @@ def main():
         curr_state.print()
     print("Game over")
     agent.print_turn_times()
+    
     
 if __name__ == "__main__":
     main()    
